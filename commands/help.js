28943521d9a7cbc306b4de38108fee388bf07c34
hotmedic2 <79211061+hotmedic2@users.noreply.github.com>
2021-06-28 00:15:58 +0600

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -39,7 +39,7 @@ module.exports = {
   Discord Music Bot Version: v${require("../package.json").version}
   [✨ Support Server](${
     client.config.SupportServer
-  }) | [GitHub](https://github.com/SudhanPlayz/Discord-MusicBot) | By [SudhanPlayz](https://github.com/SudhanPlayz)`);
+  }) | [Regal E-Sports Facebook](https://www.facebook.com/RegalEsports) | By [HotMedic2](https://www.youtube.com/watch?v=dQw4w9WgXcQ)`);
     if (!args[0]) message.channel.send(Embed);
     else {
       let cmd =
@@ -120,7 +120,7 @@ SlashCommand: {
   Discord Music Bot Version: v${require("../package.json").version}
   [✨ Support Server](${
     client.config.SupportServer
-  }) | [GitHub](https://github.com/SudhanPlayz/Discord-MusicBot) | By [SudhanPlayz](https://github.com/SudhanPlayz)`);
+  }) | [Regal E-Sports Facebook](https://www.facebook.com/RegalEsports) | By [HotMedic2](https://www.youtube.com/watch?v=dQw4w9WgXcQ)`);
       if (!args) return interaction.send(Embed);
       else {
         let cmd =
